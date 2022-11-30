a98472d17a44821e0a6347931ce9161132636539
Meera <meera.gh.barakat@gmail.com>
2022-11-30 19:29:58 +0200

    refactoring

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app=express();
-const burger=require('./burger.json');
-const menu=require('./menu.json');
+const burger=require('./data/burger.json');
+const menu=require('./data/menu.json');
 
 app.get('/menu',(req,res)=>{
     res.json(menu);
@@ -11,16 +11,9 @@ app.get('/ingredients',(req,res)=>{
     res.json(burger);
 });
 
-app.listen(4000,()=> console.log('listening on 4000...'))
-
-// const http=require('http');
-// const server=http.createServer((req,res)=>{
-//     if(req.url==='/')
-//     {
-//         res.write(JSON.stringify([1,2,3]));
-//         res.end();
-//     }
-// });
-// server.listen(4000);
-
-// console.log('listening on 4000');
+app.listen(4000,(error)=> {
+    if(!error)
+        console.log('listening on 4000...');
+    else
+        console.log("Error Occurred");
+});

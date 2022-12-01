e21d2add8718be147283f22b24a1708fbd9b038b
MeeraBarakat <meera.gh.barakat@gmail.com>
2022-12-01 16:08:59 +0200

    post-api

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 const express = require('express');
+const cors = require('cors');
+var bodyParser = require('body-parser');
 const app=express();
 const burger=require('./data/burger.json');
 const menu=require('./data/menu.json');
+
+app.use(cors());
+app.use([bodyParser.urlencoded({ extended: true, limit: '100mb' }), express.json()]);
 
 app.get('/menu',(req,res)=>{
     res.json(menu);
@@ -9,6 +14,10 @@ app.get('/menu',(req,res)=>{
 
 app.get('/ingredients',(req,res)=>{
     res.json(burger);
+});
+
+app.post('/order',(req,res)=>{
+    console.log(req.body);
 });
 
 app.listen(4000,(error)=> {
